901f26a1aa4577128172d7181b3f5f9caae078ee
unknown <ijstomazar@gmail.com>
2025-03-26 11:26:20 +0800

    reduced chunks

--- services/attendance_service.py
+++ services/attendance_service.py
@@ -56,7 +56,7 @@ def batch_insert_update_logs(db:Session, employee_logs):
     
     try:
         if inserts:
-            for batch in chunks(inserts, 1000): 
+            for batch in chunks(inserts, 100): 
                 stmt = insert(Attendance).values(batch)
                 update_dict = {
                     'time_out': stmt.inserted.time_out,
