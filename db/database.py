8f308bb6a627d91e35a84e54fbe01d17e0b68be9
unknown <ijstomazar@gmail.com>
2025-04-10 10:34:38 +0800

    increase db pool

--- db/database.py
+++ db/database.py
@@ -8,14 +8,16 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("database_url")  # REPLACE WITH DOT ENV
 
+
 engine = create_engine(
     DATABASE_URL,
-    pool_size=5,
-    max_overflow=5,
+    pool_size=10,
+    max_overflow=10,
     pool_recycle=1800,
     pool_pre_ping=True,
     connect_args={"connect_timeout": 10}
     )
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
